8a594e43eec16e88bd2fd5772f7184dca3db9304
Kaylee Sachs <hi@kaylee-sachs.dev>
2024-09-22 12:53:12 +0200

    Fixed: Wrong export syntax

--- permissions.js
+++ permissions.js
@@ -5,6 +5,8 @@ const { GuildMember, PermissionsBitField } = require('discord.js')
  * @param {GuildMember} member The user to check for sticky permissions.
  * @returns {boolean} Whether the user has permission to create a sticky.
  */
-export function hasStickyPermission(member) {
+function hasStickyPermission(member) {
   return member.permissions.has(PermissionsBitField.Flags.ManageMessages);
 }
+
+module.exports = { hasStickyPermission };
