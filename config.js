3de6097faa751784a13b3f88431170ac93f0b0d3
Kaylee Sachs <hi@kaylee-sachs.dev>
2024-09-22 12:53:20 +0200

    Added: Read local .env file

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv");
 
 dotenv.config();
+dotenv.config({ path: ".env.local", override: true });
 
 const { DISCORD_TOKEN, DISCORD_CLIENT_ID, DISCORD_WEBHOOK_NAME, DISCORD_WEBHOOK_AVATAR } = process.env;
 
